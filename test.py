ebee5f0e77b9e4010746fd0a25040abf28139c3a
joannayoo <seoyeon.yoo0117@gmail.com>
2019-10-30 08:10:46 -0400

    :bug: Updated test.py

--- test.py
+++ test.py
@@ -7,12 +7,13 @@ import time
 import random
 import numpy as np
 import torch
+import torch.nn as nn
 import torch.nn.functional as F
 from torch.utils.data.sampler import SubsetRandomSampler
 from torch.utils.data import DataLoader
 
 from data_utils import PDBBindDataset, accuracy
-from models import Model
+from models import GAGAN, GAT
 from hparams import Hparams
 
 hparams = Hparams()
@@ -34,28 +35,51 @@ if args.cuda:
 
 # Load data
 dataset = PDBBindDataset(num_positive=args.num_positive,
-                         num_negative=args.num_negative)
-dataset_size = len(dataset)
-indices = list(range(dataset_size))
-split = int(np.floor(args.train_test_split * dataset_size))
-np.random.shuffle(indices)
+                         num_negative=args.num_negative,
+                         seed=args.seed,
+                         train_test_split=args.train_test_split)
+train_indices, test_indices, val_indices = dataset.__train_test_split__()
 
-test_indices = indices[:split]
+train_sampler = SubsetRandomSampler(train_indices)
 test_sampler = SubsetRandomSampler(test_indices)
+val_sampler = SubsetRandomSampler(val_indices)
+
+train_loader = DataLoader(dataset, 
+                          #batch_size=args.batch_size,
+                          batch_size=1, # Loads graph one at a time
+                          sampler=train_sampler)
 test_loader = DataLoader(dataset, 
                          #batch_size=args.batch_size,
                          batch_size=1,
                          sampler=test_sampler)
+val_loader = DataLoader(dataset, 
+                        #batch_size=args.batch_size,
+                        batch_size=1,
+                        sampler=val_sampler)
 
-model = Model(n_out=1,
-              n_feat=dataset.__nfeats__(), 
-              n_attns=args.n_attns, 
-              n_dense=args.n_dense,
-              dim_attn=args.dim_attn,
-              dim_dense=args.dim_dense,
-              dropout=args.dropout)
+model = None
+if args.model == 'GAGAN':
+    model = GAGAN(n_out=dataset.__nlabels__(),
+                  n_feat=dataset.__nfeats__(), 
+                  n_attns=args.n_attns, 
+                  n_dense=args.n_dense,
+                  dim_attn=args.dim_attn,
+                  dim_dense=args.dim_dense,
+                  dropout=args.dropout)
+elif args.model == 'GAT':
+    model = GAT(nclass=dataset.__nlabels__(),
+                nfeat=dataset.__nfeats__(), 
+                nhid=args.dim_attn,
+                nheads=args.n_attns,
+                nhid_linear=args.dim_dense,
+                nlinear=args.n_dense,
+                alpha=args.alpha,
+                dropout=args.dropout,
+                use_distance_aware_adj=args.use_dist_aware_adj)
+else:
+    raise ValueError('args.model should be one of GAGAN, GAT')
 
-criterion = nn.BCELoss()
+criterion = nn.CrossEntropyLoss()
 
 def compute_test():
     model.eval()
@@ -65,19 +89,19 @@ def compute_test():
 
     for _ in range(len(test_loader)):
         try:
-            (X, A, D), label = next(iter(test_loader))
+            (X, A, A2), label = next(iter(test_loader))
 
             if args.cuda:
                 X = X.cuda()
                 A = A.cuda()
-                D = D.cuda()
+                A2 = A2.cuda()
                 label = label.cuda()
 
             output = model(X=X.squeeze(), 
                         A=A.squeeze(), 
-                        D=D.squeeze())
-            loss_test = criterion(output, label.float())
-            acc_test = accuracy(output, label)
+                        A2=A2.squeeze())
+            loss_test = criterion(output, label.view(-1))
+            acc_test = accuracy(output, label.view(-1))
             
             losses_batch.append(loss_test)
             acc_batch.append(acc_test)
@@ -90,6 +114,7 @@ def compute_test():
     print("Test set results:",
           "loss= {:.4f}".format(avg_loss.data),
           "accuracy= {:.4f}".format(avg_acc.data))
+
 
 # Restore best model
 model.load_state_dict(torch.load(args.model_file))
